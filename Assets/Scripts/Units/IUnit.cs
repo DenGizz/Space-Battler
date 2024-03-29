a4e070296358f015c2a409bf25cc87812a5295be
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-30 01:20:27 +0200

    Rename Entity to Unit

--- Assets/Scripts/Units/IUnit.cs
+++ Assets/Scripts/Units/IUnit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IEntity
+public interface IUnit
 {
     Vector3 Position { get; }
 }
--- Assets/Scripts/Units/UnitComponents/TestCombatUnitComponent.cs
+++ Assets/Scripts/Units/UnitComponents/TestCombatUnitComponent.cs
@@ -5,7 +5,7 @@ using Assets.Scripts.Entities;
 using UnityEngine;
 
 [AddComponentMenu("Entities/Test/TestCombatEntity")]
-public class TestCombatEntityComponent : MonoBehaviour, IEntity, IDamagable, IAttackable
+public class TestCombatUnitComponent : MonoBehaviour, IUnit, IDamagable, IAttackable
 {
     public IHealthAttribute HealthAttribute { get; private set; }
     public Vector3 Position => transform.position;
