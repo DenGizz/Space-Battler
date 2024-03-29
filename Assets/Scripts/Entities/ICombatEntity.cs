1350d692628317218f0155421739e75eb647caa5
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-30 00:34:14 +0200

    Implement IWeapon and ICombatEntity

---
+++ Assets/Scripts/Entities/ICombatEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public interface ICombatEntity
+    {
+        IEnumerable<IWeapon> Weapons { get; }
+        void Attack(IWeapon weapon, IDamagableEntity target);
+        void AddWeapon(IWeapon weapon);
+        void RemoveWeapon(IWeapon weapon);
+    }
+}
