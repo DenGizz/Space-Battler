dbf314fd626dc358611ba06c2339ce69033b2525
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-30 00:49:18 +0200

    Implement IAttackableEntity

---
+++ Assets/Scripts/Entities/IAttackableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public interface IAttackableEntity
+    {
+        IEnumerable<IWeapon> Weapons { get; }
+        void Attack(IWeapon weapon, IDamagableEntity target);
+        void AddWeapon(IWeapon weapon);
+        void RemoveWeapon(IWeapon weapon);
+    }
+}
