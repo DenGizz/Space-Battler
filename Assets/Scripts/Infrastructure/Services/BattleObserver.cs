8a8395445be2e0eb3293be502b7c91483edc8245
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:29:13 +0200

    Create BattleObserver.cs

---
+++ Assets/Scripts/Infrastructure/Services/BattleObserver.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public class BattleObserver : IBattleObserver, ITickable
+    {
+        public BattleData CurrentBattle { get; private set; }
+
+        public event Action<ISpaceShip> OnWinerDetermined;
+
+        private bool _isObserving;
+
+        public void StartObserve(BattleData battle)
+        {
+            CurrentBattle = battle;
+            _isObserving = true;
+        }
+
+        public void StopObserve()
+        {
+            _isObserving = false;
+        }
+
+        public void Tick()
+        {
+            if (_isObserving)
+                return;
+
+            if (CurrentBattle.PlayerSpaceShip.HealthAttribute.HP <= 0)
+                OnWinerDetermined?.Invoke(CurrentBattle.EnemySpaceShip);
+
+            if (CurrentBattle.EnemySpaceShip.HealthAttribute.HP <= 0)
+                OnWinerDetermined?.Invoke(CurrentBattle.PlayerSpaceShip);
+        }
+    }
+}
