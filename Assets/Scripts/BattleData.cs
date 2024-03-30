05fda1527669750b8fa86707b6208190bba28751
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:29:11 +0200

    Update BattleData.cs

--- Assets/Scripts/BattleData.cs
+++ Assets/Scripts/BattleData.cs
@@ -10,8 +10,8 @@ namespace Assets.Scripts
 {
     public class BattleData
     {
-        public ISpaceShip PlayerUnit { get; set; }
-        public ISpaceShip EnemyUnit { get; set; }
+        public ISpaceShip PlayerSpaceShip { get; set; }
+        public ISpaceShip EnemySpaceShip { get; set; }
 
         public ICombatAI PlayerAI { get;  set; }
         public ICombatAI EnemyAI { get;  set; }
@@ -24,8 +24,8 @@ namespace Assets.Scripts
         public BattleData(ISpaceShip playerUnit, ISpaceShip enemyUnit, 
             ICombatAI playerAI, ICombatAI enemyAI, bool isBattleStarted, bool isBattleEnded)
         {
-            PlayerUnit = playerUnit;
-            EnemyUnit = enemyUnit;
+            PlayerSpaceShip = playerUnit;
+            EnemySpaceShip = enemyUnit;
             PlayerAI = playerAI;
             EnemyAI = enemyAI;
             IsBattleStarted = isBattleStarted;
