dfa263cbc69a33b690ad7a379f32afeaf4f5c870
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-30 17:31:43 +0200

    Implement ServicesInstaller

---
+++ Assets/Scripts/Infrastructure/ServicesInstaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Infrastructure.Services;
+using UnityEngine;
+using Zenject;
+
+[AddComponentMenu("Infrastructure/ServicesInstaller")]
+public class ServicesInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+
+    }
+}
