58cb596148dc456ffdb6a22440f21235305c82ce
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:20:13 +0200

    Implement SpaceShipRegistry

---
+++ Assets/Scripts/Infrastructure/Services/SpaceShipRegistry.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Infrastructure.Services.Factories;
+using Assets.Scripts.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public class SpaceShipRegistry : ISpaceShipRegistry
+    {
+        public ISpaceShip PlayerSpaceShip { get; set; }
+        public ISpaceShip EnemySpaceShip { get; set; }
+    }
+}
