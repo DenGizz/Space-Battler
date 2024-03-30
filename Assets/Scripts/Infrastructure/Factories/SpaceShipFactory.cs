b19c7d3c30ad0211370c9888f33c7500400da5b3
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:20:02 +0200

    Update SpaceShipFactory.cs

--- Assets/Scripts/Infrastructure/Factories/SpaceShipFactory.cs
+++ Assets/Scripts/Infrastructure/Factories/SpaceShipFactory.cs
@@ -9,28 +9,36 @@ namespace Assets.Scripts.Infrastructure.Factories
     public class SpaceShipFactory : ISpaceShipFactory
     {
         IAssetsProvider _assetsProvider;
+        ISpaceShipRegistry _spaceShipRegistry;
 
-        public SpaceShipFactory(IAssetsProvider assetsProvider)
+        public SpaceShipFactory(IAssetsProvider assetsProvider, ISpaceShipRegistry spaceShipRegistry)
         {
             _assetsProvider = assetsProvider;
+            _spaceShipRegistry = spaceShipRegistry;
         }
 
         public ISpaceShip CreatePlayerSpaceShip(Vector3 position)
         {
             GameObject gameObject = GameObject.Instantiate(_assetsProvider.GetSpaceShipPrefab());
+            ISpaceShip spaceShip = gameObject.GetComponent<ISpaceShip>();
+
             SetSpaceShipColor(gameObject, Color.green);
-            gameObject.transform.position = position;
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
-            return gameObject.GetComponent<ISpaceShip>();
+            PlaceSpaceShip(gameObject, position, -90);
+
+            _spaceShipRegistry.PlayerSpaceShip = spaceShip;
+            return spaceShip;
         }
 
         public ISpaceShip CreateEnemySpaceShip(Vector3 position)
         {
             GameObject gameObject = GameObject.Instantiate(_assetsProvider.GetSpaceShipPrefab());
+            ISpaceShip spaceShip = gameObject.GetComponent<ISpaceShip>();
+
             SetSpaceShipColor(gameObject, Color.red);
-            gameObject.transform.position = position;
-            gameObject.transform.rotation =  Quaternion.Euler(0,0,90);
-            return gameObject.GetComponent<ISpaceShip>();
+            PlaceSpaceShip(gameObject, position, 90);
+
+            _spaceShipRegistry.EnemySpaceShip = spaceShip;
+            return spaceShip;
         }
 
         private void SetSpaceShipColor(GameObject go, Color color)
@@ -38,5 +46,10 @@ namespace Assets.Scripts.Infrastructure.Factories
             go.GetComponent<SpriteRenderer>().color = color;
         }
 
+        private void PlaceSpaceShip(GameObject go, Vector3 position, float zRotation)
+        {
+            go.transform.position = position;
+            go.transform.rotation = Quaternion.Euler(0, 0, zRotation);
+        }
     }
 }
