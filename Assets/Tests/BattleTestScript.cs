742fdc70d87050af279f5ab794ba13bf3b715c3a
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:16:13 +0200

    Update BattleTestScript.cs

--- Assets/Tests/BattleTestScript.cs
+++ Assets/Tests/BattleTestScript.cs
@@ -1,88 +1,12 @@
 using Assets.Scripts.AI;
 using Assets.Scripts.AI.UnitsAI;
 using Assets.Scripts.Units;
-using Assets.Scripts.Units.UnitComponents;
-using System.Collections;
-using System.Collections.Generic;
 using Assets.Scripts.Units.UnitAttributes;
+using Assets.Scripts.Units.UnitComponents;
 using UnityEngine;
 
 public class BattleTestScript : MonoBehaviour
 {
-    [SerializeField] private BattleInfoUIViewModel _battleInfoUi;
-
-    [SerializeField] private SpaceShipUnitComponent playerCombatUnitComponent;
-    [SerializeField] private SpaceShipUnitComponent enemyCombatUnitComponent;
-
-    [SerializeField] private AutoAttackAI playerAutoAttackAI;
-    [SerializeField] private AutoAttackAI enemyAutoAttackAI;
-
-    private ISpaceShip _playerSpaceShip;
-    private ISpaceShip _enemySpaceShip;
-
-    private ICombatAI playerAI;
-    private ICombatAI enemyAI;
-
-    private bool _isBattleStopped;
-
-    private void Start()
-    {
-        InitializeBattle();
-        StartBattle();
-    }
-
-    private void LateUpdate()
-    {
-        ObserveBattle();
-        _battleInfoUi.WriteInfo(GetBattleStatInfoString(_playerSpaceShip, _enemySpaceShip));
-    }
-
-    private void InitializeBattle()
-    {
-        _playerSpaceShip = playerCombatUnitComponent;
-        _enemySpaceShip = enemyCombatUnitComponent;
-
-        playerAI = playerAutoAttackAI;
-        enemyAI = enemyAutoAttackAI;
-
-        playerAI.SetTarget(_enemySpaceShip);
-        enemyAI.SetTarget(_playerSpaceShip);
-    }
-
-    private void StartBattle()
-    {
-        playerAI.StartCombat();
-        enemyAI.StartCombat();
-
-        _isBattleStopped = false;
-    }
-
-    private void StopBattle()
-    {
-        playerAI.StopCombat();
-        enemyAI.StopCombat();
-
-        _isBattleStopped = true;
-    }
-
-    private void ObserveBattle()
-    {
-        if (_isBattleStopped)
-            return;
-
-        if(_playerSpaceShip.HealthAttribute.HP <= 0)
-        {
-            Destroy(playerCombatUnitComponent.gameObject);
-            StopBattle();
-        }
-
-        if(_enemySpaceShip.HealthAttribute.HP <= 0)
-        {
-            Destroy(enemyCombatUnitComponent.gameObject);
-            StopBattle();
-        }
-    }
-
     private string GetBattleStatInfoString(ISpaceShip player, ISpaceShip enemy)
     {
         string GetWeaponInfo(IWeapon weapon)
@@ -106,6 +30,6 @@ public class BattleTestScript : MonoBehaviour
         string GetAttributeInfo(IHealthAttribute attribute) =>
             $"[{attribute.HP}/{attribute.BaseHP}]";
 
-       return $"Player:\n{GetUnitInfo(player)}\n\nEnemy:\n{GetUnitInfo(enemy)}";
+        return $"Player:\n{GetUnitInfo(player)}\n\nEnemy:\n{GetUnitInfo(enemy)}";
     }
 }
