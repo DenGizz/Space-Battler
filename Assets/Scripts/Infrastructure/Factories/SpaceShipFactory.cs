2f77bf02957018b5ecd08cb9a3a5ad19ca8dddd8
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 01:16:15 +0200

    Update SpaceShipFactory.cs

--- Assets/Scripts/Infrastructure/Factories/SpaceShipFactory.cs
+++ Assets/Scripts/Infrastructure/Factories/SpaceShipFactory.cs
@@ -15,19 +15,21 @@ namespace Assets.Scripts.Infrastructure.Factories
             _assetsProvider = assetsProvider;
         }
 
+        public ISpaceShip CreatePlayerSpaceShip(Vector3 position)
+        {
+            GameObject gameObject = GameObject.Instantiate(_assetsProvider.GetSpaceShipPrefab());
+            SetSpaceShipColor(gameObject, Color.green);
+            gameObject.transform.position = position;
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
+            return gameObject.GetComponent<ISpaceShip>();
+        }
+
         public ISpaceShip CreateEnemySpaceShip(Vector3 position)
         {
             GameObject gameObject = GameObject.Instantiate(_assetsProvider.GetSpaceShipPrefab());
             SetSpaceShipColor(gameObject, Color.red);
             gameObject.transform.position = position;
-            return gameObject.GetComponent<ISpaceShip>();
-        }
-
-        public ISpaceShip CreatePlayerSpaceShip(Vector3 position)
-        {         
-            GameObject gameObject = GameObject.Instantiate(_assetsProvider.GetSpaceShipPrefab());
-            SetSpaceShipColor(gameObject, Color.green);
-            gameObject.transform.position = position;
+            gameObject.transform.rotation =  Quaternion.Euler(0,0,90);
             return gameObject.GetComponent<ISpaceShip>();
         }
 
