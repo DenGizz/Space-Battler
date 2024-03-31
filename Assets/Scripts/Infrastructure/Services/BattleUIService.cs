31c40e89b6dfc112f1abf3cd0c00d8b60eb720df
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 17:54:59 +0300

    Implement BattleUIService

---
+++ Assets/Scripts/Infrastructure/Services/BattleUIService.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Infrastructure.Factories.UI_Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public class BattleUIService : IBattleUIService
+    {
+        private readonly IUIFactory _uiFactory;
+
+        private BattleUI _battleUI;
+        private GameObject _battleUIGameObject;
+
+        [Inject]
+        public BattleUIService(IUIFactory uiFactory)
+        {
+            _uiFactory = uiFactory;
+        }
+
+        public void CreateBattleUI()
+        {
+            (_battleUI, _battleUIGameObject) = _uiFactory.CreateBattleUI();
+        }
+
+        public void SetBattle(BattleData battleData)
+        {
+            _battleUI.Setup(battleData);
+        }
+
+        public void DestroyBattleUI()
+        {
+            _battleUI = null;
+            GameObject.Destroy(_battleUIGameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/IBattleUIService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public interface IBattleUIService
+    {
+        void CreateBattleUI();
+        void SetBattle(BattleData battleData);
+        void DestroyBattleUI();
+    }
+}
