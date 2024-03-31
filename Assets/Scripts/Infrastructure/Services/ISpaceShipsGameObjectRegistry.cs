799498734f2c494aeaa2174c4fd14b44f51a4655
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-03-31 17:31:38 +0300

    Implement SpaceShipsGameObjectRegistry

---
+++ Assets/Scripts/Infrastructure/Services/ISpaceShipsGameObjectRegistry.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public interface ISpaceShipsGameObjectRegistry
+    {
+        void RegisterGameObject(GameObject gameObject);
+        void UnregisterGameObject(GameObject gameObject);
+
+        GameObject GetSpaceShipGameObject(ISpaceShip spaceShip);
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/SpaceShipsGameObjectsRegistry.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public class SpaceShipsGameObjectsRegistry : ISpaceShipsGameObjectRegistry
+    {
+        private Dictionary<ISpaceShip, GameObject> _spaceShipsGameObjects;
+
+        public SpaceShipsGameObjectsRegistry()
+        {
+            _spaceShipsGameObjects = new Dictionary<ISpaceShip, GameObject>();
+        }
+
+        public GameObject GetSpaceShipGameObject(ISpaceShip spaceShip)
+        {
+            return _spaceShipsGameObjects[spaceShip];
+        }
+
+        public void RegisterGameObject(GameObject gameObject)
+        {
+            _spaceShipsGameObjects.Add(gameObject.GetComponent<ISpaceShip>(), gameObject);
+        }
+
+        public void UnregisterGameObject(GameObject gameObject)
+        {
+           _spaceShipsGameObjects.Remove(gameObject.GetComponent<ISpaceShip>());
+        }
+    }
+}
