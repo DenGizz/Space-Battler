6eaff1735085b9cc7a59f42dce701a2a6834db27
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-01 17:55:33 +0300

    Update GameBootstrapper.cs

--- Assets/Scripts/Infrastructure/Game/GameBootstrapper.cs
+++ Assets/Scripts/Infrastructure/Game/GameBootstrapper.cs
@@ -1,10 +1,28 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using Zenject;
 
 namespace Assets.Scripts.Infrastructure.Game
 {
+    [AddComponentMenu("Infrastructure/GameBootstrapper")]
     public class GameBootstrapper : MonoBehaviour
     {
+        private Game _game;
 
+        [Inject]
+        public void Construct(Game game)
+        {
+            _game = game;
+        }
+
+        private void LunchGame()
+        {
+            _game.Start();
+        }
+
+        private void Awake()
+        {
+            LunchGame();
+        }
     }
 }
