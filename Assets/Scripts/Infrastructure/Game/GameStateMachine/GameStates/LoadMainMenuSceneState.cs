204115463f0a0785c307bb19b51a26cded273fc4
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-02 20:50:41 +0300

    Created MainMEnuStatte and LoadMainMenuState

---
+++ Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/LoadMainMenuSceneState.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Infrastructure.Config;
+using Assets.Scripts.Infrastructure.Services.CoreServices;
+using Assets.Scripts.StateMachine;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure.Game.GameStateMachine.GameStates
+{
+    public class LoadMainMenuSceneState : IState
+    {
+        private readonly GameStateMachine _gameStateMachine;
+        private readonly ISceneLoader _sceneLoader;
+        private readonly ScenesConfig _scenesConfig;
+
+        public LoadMainMenuSceneState(GameStateMachine gameStateMachine, ISceneLoader sceneLoader, ScenesConfig scenesConfig)
+        {
+            _gameStateMachine = gameStateMachine;
+            _sceneLoader = sceneLoader;
+            _scenesConfig = scenesConfig;
+        }
+
+        public void Enter()
+        {
+            _sceneLoader.LoadSceneAsync(_scenesConfig.MainMenuSceneName, () => _gameStateMachine.EnterState<MainMenuState>());
+        }
+
+        public void Exit()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/MainMenuState.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrastructure.Game.GameStateMachine.GameStates
+{
+    public class MainMenuState : IState
+    {
+        private GameStateMachine _gameStateMachine;
+
+        public MainMenuState(GameStateMachine gameStateMachine)
+        {
+            _gameStateMachine = gameStateMachine;
+        }
+
+        public void Enter()
+        {
+
+        }
+
+        public void Exit()
+        {
+
+        }
+    }
+}
