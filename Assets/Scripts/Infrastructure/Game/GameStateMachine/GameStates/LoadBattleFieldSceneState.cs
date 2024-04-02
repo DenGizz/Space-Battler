6e78009e306ff1cca53ae9c2b162dd383cb168e3
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-02 15:51:40 +0300

    Update LoadBattleFieldSceneState.cs

--- Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/LoadBattleFieldSceneState.cs
+++ Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/LoadBattleFieldSceneState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Assets.Scripts.Infrastructure.Services;
 using Assets.Scripts.StateMachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,17 +10,20 @@ namespace Assets.Scripts.Infrastructure.Game.GameStateMachine.GameStates
     {
         private readonly GameStateMachine GameStateMachine;
         private readonly ScenesConfig _scenesConfig;
+        private readonly ISceneLoader _sceneLoader;
 
-        public LoadBattleFieldSceneState(GameStateMachine gameStateMachine, ScenesConfig scenesConfig)
+        public LoadBattleFieldSceneState(GameStateMachine gameStateMachine, ScenesConfig scenesConfig, ISceneLoader sceneLoader)
         {
             GameStateMachine = gameStateMachine;
             _scenesConfig = scenesConfig;
+            _sceneLoader = sceneLoader;
         }
 
         public void Enter()
         {
-            SceneManager.LoadScene(_scenesConfig.BattleFieldSceneName, LoadSceneMode.Single);
-            GameStateMachine.EnterState<CreateBattleState>();
+            _sceneLoader.LoadSceneAsync
+                (_scenesConfig.BattleFieldSceneName, 
+                () => GameStateMachine.EnterState<CreateBattleState>());
         }
 
         public void Exit()
