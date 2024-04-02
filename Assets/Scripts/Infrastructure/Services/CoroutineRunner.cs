c2ccd58b947a34e930c0b50cfa604a359210ee71
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-02 15:51:31 +0300

    Implement CoroutineRunner

---
+++ Assets/Scripts/Infrastructure/Services/CoroutineRunner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Unity.Collections.Unicode;
+
+namespace Assets.Scripts.Infrastructure.Services
+{
+    public class CoroutineRunner : ICoroutineRunner
+    {
+        private static CoroutineRunnerComponent runner;
+
+        public CoroutineRunner()
+        {
+            GameObject runnerObject = new GameObject("CoroutineRunner");
+            CoroutineRunnerComponent runnerComponent = runnerObject.AddComponent<CoroutineRunnerComponent>();
+            GameObject.DontDestroyOnLoad(runnerObject);
+            runner = runnerObject.AddComponent<CoroutineRunnerComponent>();
+        }
+
+        public Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+            return runner.StartCoroutine(coroutine);
+        }
+
+        public void StopCoroutine(Coroutine coroutine)
+        {
+            runner.StopCoroutine(coroutine);
+        }
+    }
+
+    public class CoroutineRunnerComponent : MonoBehaviour { }
+}
---
+++ Assets/Scripts/Infrastructure/Services/ICoroutineRunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure
+{
+    public interface ICoroutineRunner
+    {
+        public Coroutine StartCoroutine(IEnumerator coroutine);
+        public void StopCoroutine(Coroutine coroutine);
+    }
+}
