e4c40f0a928258946ac66e43b21cee3efed6cd2c
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-02 15:12:42 +0300

    Implement LoadBattleFieldSceneState

---
+++ Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/LoadBattleFieldSceneState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using Assets.Scripts.StateMachine;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure.Game.GameStateMachine.GameStates
+{
+    public class LoadBattleFieldSceneState : IState
+    {
+        private readonly GameStateMachine GameStateMachine;
+
+        public LoadBattleFieldSceneState(GameStateMachine gameStateMachine)
+        {
+            GameStateMachine = gameStateMachine;
+        }
+
+        public void Enter()
+        {
+            LoadBattleFieldScene();
+            GameStateMachine.EnterState<CreateBattleState>();
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        private void LoadBattleFieldScene()
+        {
+
+        }
+    }
+}
