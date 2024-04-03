901e21e659d7e498e622d4645596bd716f15a175
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-03 14:14:20 +0300

    Update MainMenuState.cs

--- Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/MainMenuState.cs
+++ Assets/Scripts/Infrastructure/Game/GameStateMachine/GameStates/MainMenuState.cs
@@ -1,29 +1,41 @@
-﻿using Assets.Scripts.StateMachine;
+﻿using Assets.Scripts.Infrastructure.Factories.UI_Factories;
+using Assets.Scripts.StateMachine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace Assets.Scripts.Infrastructure.Game.GameStateMachine.GameStates
 {
     public class MainMenuState : IState
     {
-        private GameStateMachine _gameStateMachine;
+        private readonly GameStateMachine _gameStateMachine;
+        private readonly IUIFactory _uiFactory;
 
-        public MainMenuState(GameStateMachine gameStateMachine)
+        private MainMenuUI _mainMenuUI;
+
+        public MainMenuState(GameStateMachine gameStateMachine, IUIFactory uiFactory)
         {
             _gameStateMachine = gameStateMachine;
+            _uiFactory = uiFactory;
         }
 
         public void Enter()
         {
-
+            _mainMenuUI = _uiFactory.CreateMainMenuUI();
+            _mainMenuUI.OnStartBattleButtonClicked += OnStartBattleButtonClicked;
         }
 
         public void Exit()
         {
 
+        }
+
+        private void OnStartBattleButtonClicked()
+        {
+            _gameStateMachine.EnterState<LoadBattleFieldSceneState>();
         }
     }
 }
