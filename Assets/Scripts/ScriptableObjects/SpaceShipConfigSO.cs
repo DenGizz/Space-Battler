f3741abebc45b26fa763af763f8a5c58836e5155
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-07 11:22:50 +0300

    Update SpaceShipConfigSO.cs

--- Assets/Scripts/ScriptableObjects/SpaceShipConfigSO.cs
+++ Assets/Scripts/ScriptableObjects/SpaceShipConfigSO.cs
@@ -15,10 +15,12 @@ namespace Assets.Scripts.ScriptableObjects
         public SpaceShipType CorpusType  => _corpusType;
         public float MaxHealth  => _maxHealth;
         public int WeaponSlotsCount => _weaponSlotsCount;
+        public Sprite Sprite => _sprite;
 
         [SerializeField] private SpaceShipType _corpusType;
         [SerializeField] private float _maxHealth;
         [SerializeField] private int _weaponSlotsCount;
+        [SerializeField] private Sprite _sprite;
 
 
         public SpaceShipConfig GetSpaceShipConfig()
