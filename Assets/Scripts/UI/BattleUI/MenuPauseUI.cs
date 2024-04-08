695b4008b5b4372cad23750e6bd485b66b082e8d
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-08 22:37:23 +0300

    Refactor UI prefabs folder

---
+++ Assets/Scripts/UI/BattleUI/MenuPauseUI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPauseUI : MonoBehaviour
+{
+    public Action OnPauseContinueButtonClicked;
+
+    [SerializeField] private Button _pauseContinueButton;
+    
+    private void Awake()
+    {
+        _pauseContinueButton.onClick.AddListener(OnPauseContinueButtonClickedEventHandler);
+    }
+
+    private void OnPauseContinueButtonClickedEventHandler()
+    {
+        OnPauseContinueButtonClicked?.Invoke();
+    }
+}
