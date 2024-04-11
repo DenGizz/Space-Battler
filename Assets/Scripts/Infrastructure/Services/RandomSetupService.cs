7f140f561be5688c153210a1a7a38eff1cc49b55
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-11 19:17:31 +0300

    Update RandomSetupService.cs

--- Assets/Scripts/Infrastructure/Services/RandomSetupService.cs
+++ Assets/Scripts/Infrastructure/Services/RandomSetupService.cs
@@ -1,11 +1,7 @@
 ﻿using Assets.Scripts.Infrastructure.Services.CoreServices;
 using Assets.Scripts.ScriptableObjects;
 using Assets.Scripts.SpaceShips.SpaceShipConfigs;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Zenject;
 
 namespace Assets.Scripts.Infrastructure.Services
@@ -25,8 +21,10 @@ namespace Assets.Scripts.Infrastructure.Services
             SpaceShipDescriptor[] spaceShipDescriptors = _staticDataService.GetSpaceShipDescriptors().ToArray();
             WeaponDescriptor[] weaponDescriptors = _staticDataService.GetWeaponDescriptors().ToArray();
 
-            SpaceShipDescriptor randomSpaceShipDescriptor = spaceShipDescriptors[UnityEngine.Random.Range(0, spaceShipDescriptors.Length)];
-            WeaponDescriptor[] randomWeaponDescriptors = weaponDescriptors.OrderBy(weaponDescriptor => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 4)).ToArray();
+            int randomSpaceShipDescriptorIndex = UnityEngine.Random.Range(0, spaceShipDescriptors.Length);
+            SpaceShipDescriptor randomSpaceShipDescriptor = spaceShipDescriptors[randomSpaceShipDescriptorIndex];
+            WeaponDescriptor[] randomWeaponDescriptors = weaponDescriptors
+                .OrderBy(weaponDescriptor => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 4)).ToArray();
             return new SpaceShipSetup(randomSpaceShipDescriptor.CorpusType, randomWeaponDescriptors.Select(weaponDescriptor => weaponDescriptor.WeaponType));
         }
     }
