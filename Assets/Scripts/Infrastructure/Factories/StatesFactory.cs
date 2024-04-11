7b575c3734fefcb6eb357b2f0edce9e67c6b0893
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-11 19:13:46 +0300

    Update StatesFactory.cs

--- Assets/Scripts/Infrastructure/Factories/StatesFactory.cs
+++ Assets/Scripts/Infrastructure/Factories/StatesFactory.cs
@@ -1,9 +1,4 @@
 ﻿using Assets.Scripts.StateMachines;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Zenject;
 
 namespace Assets.Scripts.Infrastructure.Factories
