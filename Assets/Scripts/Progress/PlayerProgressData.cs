470caeff746558383168c192fc5700592ebb76ae
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-30 19:09:12 +0300

    Implement PlayerProgressData

---
+++ Assets/Scripts/Progress/PlayerProgressData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Progress
+{
+    public class PlayerProgressData
+    {
+        public int BattlesWon { get; set; }
+        public int BattlesLost { get; set; }
+
+        public PlayerProgressData()
+        {
+            BattlesWon = 0;
+            BattlesLost = 0;
+        }
+
+        public PlayerProgressData(int battlesWon, int battlesLost)
+        {
+            BattlesWon = battlesWon;
+            BattlesLost = battlesLost;
+        }
+    }
+}
