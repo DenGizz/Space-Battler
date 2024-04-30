434a97cd93b11652ce78c2693c07622f96bb59c5
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-30 19:56:49 +0300

    Fix null winner bug

--- Assets/Scripts/Game/GameStates/BattleState.cs
+++ Assets/Scripts/Game/GameStates/BattleState.cs
@@ -63,6 +63,7 @@ namespace Assets.Scripts.Game.GameStates
         private void OnWinnerDeterminedEventHandler(ISpaceShip winner)
         {
             StopBattle();
+            _battle.Winner = winner;
             UpdateAndSaveProgress(winner);
 
 
