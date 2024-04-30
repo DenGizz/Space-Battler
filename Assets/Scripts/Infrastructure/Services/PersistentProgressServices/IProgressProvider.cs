5c76dd4af81319b6ccf8d1aa4b1f93cd9923355f
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-30 19:09:23 +0300

    Implement ProgressProvider

---
+++ Assets/Scripts/Infrastructure/Services/PersistentProgressServices/IProgressProvider.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Progress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrastructure.Services.PersistentProgressServices
+{
+    public interface IProgressProvider
+    {
+        PlayerProgressData PlayerProgressData { get; set; }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/PersistentProgressServices/ProgressProvider.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Infrastructure.Services.CoreServices;
+using Assets.Scripts.Progress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrastructure.Services.PersistentProgressServices
+{
+    public class ProgressProvider : IProgressProvider
+    {
+        public PlayerProgressData PlayerProgressData { get; set; }
+    }
+}
