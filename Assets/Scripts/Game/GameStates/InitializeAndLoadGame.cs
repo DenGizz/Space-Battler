807a1d3a2a0fd6f01d00ead497908157a43a902c
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-04-30 19:09:46 +0300

    Update InitializeAndLoadGame.cs

--- Assets/Scripts/Game/GameStates/InitializeAndLoadGame.cs
+++ Assets/Scripts/Game/GameStates/InitializeAndLoadGame.cs
@@ -1,6 +1,8 @@
 ﻿using Assets.Scripts.Battles;
 using Assets.Scripts.Infrastructure.Services;
 using Assets.Scripts.Infrastructure.Services.CoreServices;
+using Assets.Scripts.Infrastructure.Services.PersistentProgressServices;
+using Assets.Scripts.Progress;
 using Assets.Scripts.SpaceShips.SpaceShipConfigs;
 using Assets.Scripts.StateMachines;
 using System;
@@ -15,13 +17,15 @@ namespace Assets.Scripts.Game.GameStates
     {
         private readonly StateMachine _stateMachine;
         private readonly IBattleSetupProvider _battleSetupProvider;
+        private readonly IProgressProvider _progressProvider;
         private readonly IPersistentDataService _persistentDataService;
 
-        public InitializeAndLoadGame(StateMachine stateMachine, IBattleSetupProvider battleSetupProvider, IPersistentDataService persistentDataService)
+        public InitializeAndLoadGame(StateMachine stateMachine, IBattleSetupProvider battleSetupProvider, IPersistentDataService persistentDataService, IProgressProvider progressProvider)
         {
             _stateMachine = stateMachine;
             _battleSetupProvider = battleSetupProvider;
             _persistentDataService = persistentDataService;
+            _progressProvider = progressProvider;
         }
 
         public void Enter()
@@ -29,6 +33,7 @@ namespace Assets.Scripts.Game.GameStates
             _persistentDataService.Initialize();
 
             LoadOrCreateBattleSetup();
+            LoadOrCreateProgress();
             _stateMachine.EnterState<LoadMainMenuSceneState>();
         }
 
@@ -51,6 +56,20 @@ namespace Assets.Scripts.Game.GameStates
                 BattleSetup battleSetup = new BattleSetup(playerSetup, enemySetup);
                 _persistentDataService.SaveBattleSetup(battleSetup);
                 _battleSetupProvider.BattleSetup = battleSetup;
+            }
+        }
+
+        private void LoadOrCreateProgress()
+        {
+            if(_persistentDataService.IsPlayerProgressDataStored())
+            {
+                _progressProvider.PlayerProgressData = _persistentDataService.LoadPlayerProgressData();
+            }
+            else
+            {
+                PlayerProgressData playerProgressData = new PlayerProgressData();
+                _persistentDataService.SavePlayerProgressData(playerProgressData);
+                _progressProvider.PlayerProgressData = playerProgressData;
             }
         }
     }
