581c6c0e3d383b223240d199420dcec4325fd1d2
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-05-02 15:36:17 +0300

    Fix RandomizeSetupService

--- Assets/Scripts/Infrastructure/Services/RandomSetupService.cs
+++ Assets/Scripts/Infrastructure/Services/RandomSetupService.cs
@@ -3,6 +3,8 @@ using Assets.Scripts.ScriptableObjects;
 using System.Linq;
 using Assets.Scripts.Entities.SpaceShips.SpaceShipConfigs;
 using Zenject;
+using Assets.Scripts.Entities.Weapons.WeaponConfigs;
+using Unity.VisualScripting;
 
 namespace Assets.Scripts.Infrastructure.Services
 {
@@ -18,18 +20,37 @@ namespace Assets.Scripts.Infrastructure.Services
 
         public void RandomizeSpaceShipSetup(SpaceShipSetup spaceShipSetup)
         {
-            SpaceShipDescriptor[] spaceShipDescriptors = _staticDataService.GetSpaceShipDescriptors().ToArray();
-            WeaponDescriptor[] weaponDescriptors = _staticDataService.GetWeaponDescriptors().ToArray();
+            SpaceShipType randomSpaceShipType = PickRandomSpaceShipType();
 
-            int randomSpaceShipDescriptorIndex = UnityEngine.Random.Range(0, spaceShipDescriptors.Length);
-            SpaceShipDescriptor randomSpaceShipDescriptor = spaceShipDescriptors[randomSpaceShipDescriptorIndex];
-            WeaponDescriptor[] randomWeaponDescriptors = weaponDescriptors
-                .OrderBy(weaponDescriptor => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 4)).ToArray();
+            int awaliableWeaponSlots = _staticDataService.GetSpaceShipDescriptor(randomSpaceShipType).WeaponSlotsCount;
 
-            spaceShipSetup.SpaceShipType = randomSpaceShipDescriptor.SpaceShipType;
+
+            var randomWeaponTypes = PickRandomWeaponTypes(awaliableWeaponSlots);
+
+            spaceShipSetup.SpaceShipType = randomSpaceShipType;
             spaceShipSetup.WeaponTypes.Clear();
-            spaceShipSetup.WeaponTypes.AddRange(randomWeaponDescriptors.Select(weaponDescriptor => weaponDescriptor.WeaponType));
+            spaceShipSetup.WeaponTypes.AddRange(randomWeaponTypes);
+        }
 
+        private  SpaceShipType PickRandomSpaceShipType()
+        {
+            SpaceShipDescriptor[] spaceShipDescriptors = _staticDataService.GetSpaceShipDescriptors().ToArray();
+            int randomSpaceShipDescriptorIndex = UnityEngine.Random.Range(0, spaceShipDescriptors.Length);
+            return spaceShipDescriptors[randomSpaceShipDescriptorIndex].SpaceShipType;
+        }
+
+        private WeaponType[] PickRandomWeaponTypes(int count)
+        {
+            WeaponDescriptor[] randomWeaponDescriptors = _staticDataService.GetWeaponDescriptors().ToArray();
+            WeaponType[] randomWeaponTypes = new WeaponType[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int randomWeaponDescriptorIndex = UnityEngine.Random.Range(0, randomWeaponDescriptors.Length);
+                randomWeaponTypes[i] = randomWeaponDescriptors[randomWeaponDescriptorIndex].WeaponType;
+            }
+
+            return randomWeaponTypes;
         }
     }
 }
