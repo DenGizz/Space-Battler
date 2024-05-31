33b1510a51d3e94f6c0217f5555262dd63110ed5
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-05-31 12:19:24 +0300

    Update ItemSlotViewModel.cs

--- Assets/Scripts/UI/ViewModels/SlotViewModels/ItemSlotViewModel.cs
+++ Assets/Scripts/UI/ViewModels/SlotViewModels/ItemSlotViewModel.cs
@@ -13,10 +13,21 @@ namespace Assets.Scripts.UI.ViewModels.SlotViewModels
     public abstract class ItemSlotViewModel<TOption> : MonoBehaviour
     {
         public IEnumerable<TOption> Options { get; set; }
-        public virtual TOption SelectedOption { get; set; }
+        public virtual TOption SelectedOption
+        {
+            get => _selectedOption;
+            set
+            {
+                _selectedOption = value;
+                OnOptionSelected?.Invoke(value);
+            }
+        }
+
         public TOption DefaultOption { get; set; }
 
         public event Action<TOption> OnOptionSelected;
+
+        private TOption _selectedOption;
 
         public void SetOptions(IEnumerable<TOption> options, TOption defaultOption)
         {
