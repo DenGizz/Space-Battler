0de75221557e1dd057f0696d56184f0de3f72004
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-06-03 17:46:48 +0300

    Update BattleSetup.cs

--- Assets/Scripts/Game/SandboxMode/BattleSetups/BattleSetup.cs
+++ Assets/Scripts/Game/SandboxMode/BattleSetups/BattleSetup.cs
@@ -1,9 +1,13 @@
 ﻿using Assets.Scripts.Entities.SpaceShips.SpaceShipConfigs;
+using Assets.Scripts.Game.SandboxMode.BattleSetups;
 
 namespace Assets.Scripts.Battles
 {
     public class BattleSetup
     {
+        public BattleTeamSetup PlayerTeamSetup { get; set; }
+        public BattleTeamSetup EnemyTeamSetup { get; set; }
+
         public SpaceShipSetup PlayerSetup { get; set; }
         public SpaceShipSetup EnemySetup { get; set; }
 
