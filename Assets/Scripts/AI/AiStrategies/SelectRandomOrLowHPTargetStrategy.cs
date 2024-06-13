94953a9bdfd6347dd5c26f00843a9e9b45036231
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-06-13 18:54:43 +0300

    Create SelectRandomOrLowHPTargetStrategy.cs

---
+++ Assets/Scripts/AI/AiStrategies/SelectRandomOrLowHPTargetStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Entities.SpaceShips;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.AI.AiStrategies
+{
+    public class SelectRandomOrLowHPTargetStrategy : ISelectTargetStrategy
+    {
+        private const float _lowHPThreshold = 0.25f;
+
+        public ISpaceShip SelectTarget(IEnumerable<ISpaceShip> spaceShips)
+        {
+            var target = spaceShips.Where(t => t.Config.MaxHP / t.Data.HealthPoints < _lowHPThreshold).FirstOrDefault();
+
+            return target == default ? target : spaceShips.OrderBy(x => x.Data.HealthPoints).FirstOrDefault();
+
+        }
+    }
+}
